4373ac0aa5004380a3b7d06a1ea087eedf962c4c
Adarsh Wilson <adarshwilsona@gmail.com>
2024-02-16 17:07:18 +0530

    api calls belong to the backend not the frontend lol

--- backend-server/index.js
+++ backend-server/index.js
@@ -1,11 +1,51 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const US_API = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=09aae49f3e934487ab282649b8812c25'
+const BBC_API = 'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=09aae49f3e934487ab282649b8812c25'
+const GERMANY_API = 'https://newsapi.org/v2/top-headlines?country=de&category=business&apiKey=09aae49f3e934487ab282649b8812c25'
+const TRUMP_API = 'https://newsapi.org/v2/top-headlines?q=trump&apiKey=09aae49f3e934487ab282649b8812c25'
+
+
+app.get('/', async (req, res) => {
+    res.send("hello world!")
+});
+
+/*
+    us
+    bbc
+    germany 
+    trump
+*/
+
+app.get('/us', (req, res) => {
+    fetchAPI(US_API, res);
+});
+
+app.get('/bbc', (req, res) => {
+    fetchAPI(BBC_API, res);
+});
+
+app.get('/germany', (req, res) => {
+    fetchAPI(GERMANY_API, res);
+});
+
+app.get('/trump', (req, res) => {
+    fetchAPI(TRUMP_API, res);
 });
 
 app.listen(port, () => {
   console.log(`SERVER RUNNING ON PORT ${port}`);
 });
+
+const fetchAPI = async (api, res) => {
+    try {
+        const response = await axios.get(api);
+        res.send(response.data.articles.slice(0, 5));
+      } catch (error) {
+        console.error(error);
+        res.status(500).send(`NODE SERVER ERROR : couldnt fetch headlines. for ${api}`);
+      }
+}
--- src/NewsHeadlines.jsx
+++ src/NewsHeadlines.jsx
@@ -1,37 +1,9 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 const NewsHeadlines = (props) => {
-  const [headlines, setHeadlines] = useState([]);
-  const [error, setError] = useState(null);
-
-  //xd?
-  useEffect(() => {
-    axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=09aae49f3e934487ab282649b8812c25', {
-      headers: {
-        'X-Api-Key': '09aae49f3e934487ab282649b8812c25'
-      }
-    })// 
-    .then(response => {
-      setHeadlines(response.data.articles.slice(0, 5));
-    })
-    .catch(error => {
-      setError('ERROR : couldnt fetch headlines.');
-    });
-  }, []);
-
-  return (
-    <div>
-      {error && <p>{error}</p>} 
-
-      {headlines.map((headline, i) => (
-        <div key={i}>
-          <h2>{headline.title}</h2>
-          <p>{headline.source.name}</p>
-          <p>{headline.publishedAt}</p>
-        </div>
-      ))}
-    </div>
+  
+  return(
+    <div>xd</div>
   );
 };
 
